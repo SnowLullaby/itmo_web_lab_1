6033ac273eed71e13867c80f97c488e2d8ebff0f
Екатерина <katish.vav@gmail.com>
2025-07-06 03:46:44 +0300

    structure fix

--- src/main/java/ru/web/parser/Parser.java
+++ src/main/java/ru/web/parser/Parser.java
@@ -7,13 +7,13 @@ import ru.web.dto.RequestDTO;
 public class Parser {
     private static final Gson GSON = new Gson();
 
-    public static ParseResult parse(String jsonString) throws IllegalArgumentException {
+    public static RequestDTO parse(String jsonString) throws IllegalArgumentException {
         try {
             RequestDTO dto = GSON.fromJson(jsonString, RequestDTO.class);
             if (dto == null) {
                 throw new IllegalArgumentException("Пустой JSON");
             }
-            return new ParseResult(dto);
+            return dto;
         } catch (JsonSyntaxException e) {
             throw new IllegalArgumentException("Некорректный формат запроса", e);
         }
--- src/main/java/ru/web/validators/JsonValidator.java
+++ src/main/java/ru/web/validators/JsonValidator.java
@@ -1,18 +1,18 @@
 package ru.web.validators;
 
-import ru.web.parser.ParseResult;
+import ru.web.dto.RequestDTO;
 
 public class JsonValidator {
-    public static ValidationResult validate(ParseResult parsed) {
-        if (parsed.getX() < -2 || parsed.getX() > 2) {
+    public static ValidationResult validate(RequestDTO parsed) {
+        if (parsed.x() < -2 || parsed.x() > 2) {
             return new ValidationResult(false, "X должен быть числом от -2 до 2");
         }
 
-        if (parsed.getY() < -5 || parsed.getY() > 5) {
+        if (parsed.y() < -5 || parsed.y() > 5) {
             return new ValidationResult(false, "Y должен быть числом от -5 до 5");
         }
 
-        for (double r : parsed.getR()) {
+        for (double r : parsed.r()) {
             if (r < 1 || r > 3) {
                 return new ValidationResult(false, "R должен быть числом от 1 до 3");
             }
